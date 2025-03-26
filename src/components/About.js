ae7f0ee9d96ee12912942f8a47e4182a5e3c5b9c
rafikiifca <rafaeljuarezvelazquez@comunidad.unam.mx>
2025-03-25 21:03:02 -0600

    Pratica6

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,14 @@
+import PokemonCard from "./PokemonCard";
+
 const About = () =>{
-    <p>relleno</p>
+    const data = {"count":1302,"items":[{"id":1,"name":"bulbasaur","weight":69,"types":[{"type":{"name":"grass","url":"https://pokeapi.co/api/v2/type/12/"},"slot":1},{"type":{"name":"poison","url":"https://pokeapi.co/api/v2/type/4/"},"slot":2}],"abilities":[{"ability":{"name":"overgrow","url":"https://pokeapi.co/api/v2/ability/65/"},"is_hidden":false,"slot":1},{"ability":{"name":"chlorophyll","url":"https://pokeapi.co/api/v2/ability/34/"},"is_hidden":true,"slot":3}],"sprites":{"front_default":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"}}]}
+
+    return(
+        <div>
+            {data.items.map((item, ) => (<PokemonCard item = {item}/> ))}
+
+        </div>
+    )
 }
 
 export default About;

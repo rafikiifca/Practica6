0e07c52496b3a473315b0d84e2365d2b69cd14a0
rafikiifca <rafaeljuarezvelazquez@comunidad.unam.mx>
2025-03-27 20:23:06 -0600

    Pokedex y PokemonCard

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,30 +1,31 @@
 import {Box, Button, Card, CardActionArea, CardActions, CardContent, Typography} from "@mui/material";
 
-const PokemonCard = ({item}) => {
+const PokemonCard = ({item}) =>{
     return (<Card sx={{ maxWidth: 345 }}>
-        <CardActionArea>
-            <Box>
-                <img src={item.sprites.front_default}/>
-            </Box>
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    nombre: {item.name}
-                </Typography>
-                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                    peso: {item.weight}
-                </Typography>
-                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                    tipo: {item.types[0].type.name}
-                </Typography>
-            </CardContent>
-        </CardActionArea>
-        <CardActions>
-            <Button size="small" color="primary">
-                Share
-            </Button>
-        </CardActions>
-    </Card>
+            <CardActionArea>
+                <Box>
+                    <img src={item.sprites.front_default}/>
+                </Box>
 
-    )
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        nombre: {item.name}
+                    </Typography>
+                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                        peso: {item.weight}
+                    </Typography>
+                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                        tipo:{item.types[0].type.name}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary">
+                    Share
+                </Button>
+            </CardActions>
+        </Card>
+
+    );
 }
 export default PokemonCard;
